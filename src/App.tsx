942f3503044f580b62a7395a455557aa64e86646
Kunal Singh <16257541+velociraptor98@users.noreply.github.com>
2023-06-06 22:55:41 +0100

    Project front end setup

--- src/App.tsx
+++ src/App.tsx
@@ -18,20 +18,9 @@ export const App = () => (
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
         <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
+          <Logo h="20vmin" pointerEvents="none" />
         </VStack>
+        <Text><Code>Development Place Holder</Code></Text>
       </Grid>
     </Box>
   </ChakraProvider>
