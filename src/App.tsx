bf8c805919a7987ae2fe4acb963e29635b7871e4
Kunal Singh <16257541+velociraptor98@users.noreply.github.com>
2023-09-23 18:17:12 +0100

    adding state handling for event form

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
-import * as React from "react"
 import {
   ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  Container,
   Center,
 } from "@chakra-ui/react"
+
 import { Navbar } from "../src/Components/Nav/Navbar";
 import { NewEvent } from "./Components/Forms/NewEvent";
 import { SocialCard } from "./Components/NewEvents/SocialCard";
@@ -20,7 +13,7 @@ export const App = () => (
   <ChakraProvider theme={theme}>
     <Navbar/>
     <Center bgColor='#E2E2E2' minHeight="100vh" minWidth="100vw">
-    <SocialCard/>
+    <NewEvent/>
     </Center>
   </ChakraProvider>
 )
--- src/Components/Forms/NewEvent.tsx
+++ src/Components/Forms/NewEvent.tsx
@@ -10,52 +10,85 @@ import {
     Spacer,
 } from '@chakra-ui/react'
 import './NewEvent.css'
-import { useState,useEffect } from 'react'
+import { useState, ChangeEvent } from 'react'
 
 export const NewEvent = () => {
-    const [eventName,setEventName] = useState("Placeholder");
+    const [eventName,setEventName] = useState("");
     const [eventDate,setEventDate] = useState("01/01/2020");
     const [startTime,setStartTime] = useState('2100');
     const [endTime,setEndTime] = useState('2300');
-    const [host,setHost] = useState('Johm');
+    const [host,setHost] = useState('');
     const [eventLink,setEventLink] = useState('test.huddle01');
     const [customUrl,setCustomUrl] = useState('www.test.some.io');
-    const [currency,setCurrency] = useState(0);
+    const [currency,setCurrency] = useState("");
+    const [sellerName,setSellerName] = useState('');
+    const [floorPrice,setFloorPrice] = useState(0);
+
+    const changeEventName = (e: ChangeEvent<HTMLInputElement>) => {
+        setEventName(e.target.value);
+    }
+    const changeSellerName = (e: ChangeEvent<HTMLInputElement>) => {
+        setSellerName(e.target.value);
+    }
+    const changeFloorPrice = (e: ChangeEvent<HTMLInputElement>) => {
+        setFloorPrice(parseInt(e.target.value));
+    }
+
+    const changeStartTime = (e: ChangeEvent<HTMLInputElement>) => {
+        setStartTime(e.target.value);
+    }
+
+    const changeEndTime = (e: ChangeEvent<HTMLInputElement>) => {
+        setEndTime(e.target.value);
+    }
+
+    const changeCurrency = (e: ChangeEvent<HTMLInputElement>) => {
+        setCurrency(e.target.value);
+    }
+
+    const changeHuddleLink = (e: ChangeEvent<HTMLInputElement>) => {
+        setEventLink(e.target.value);
+    }
+
+    const changeCustomURL = (e: ChangeEvent<HTMLInputElement>) => {
+        setCustomUrl(e.target.value);
+    }
+
     return (
         <Flex className='form-container' height='624px' width='800px'>
             <Container className='form-subcontainer' centerContent={true}>
             <Text color='white' fontSize='3xl'>Create New Event</Text>
             <FormControl>
                 <FormLabel fontSize='sm' color='#FFFFFFA6'>Event Name</FormLabel>
-                <Input type="text" color='#FFFFFFA6' placeholder='Name this event' borderRadius = '15px'></Input>
+                <Input onChange = {changeEventName} value = {eventName} type="text" color='#FFFFFFA6' placeholder='Name this event' borderRadius = '15px'></Input>
                 <FormLabel fontSize='sm' color='#FFFFFFA6'>Seller's Name</FormLabel>
-                <Input type="text" color='#FFFFFFA6' placeholder='Who is conducting this event' borderRadius = '15px'></Input>
+                <Input onChange = {changeSellerName} value = {sellerName} type="text" color='#FFFFFFA6' placeholder='Who is conducting this event' borderRadius = '15px'></Input>
                 <FormLabel fontSize='sm' color='#FFFFFFA6'>Date of Event</FormLabel>
                 <Input type="date" color='#FFFFFFA6' placeholder='Pick a date for this event' borderRadius = '15px'></Input>
                 <HStack>
                     <Stack>
                     <FormLabel fontSize='sm' color='#FFFFFFA6'>Start Time</FormLabel>
-                    <Input type="date" color='#FFFFFFA6' placeholder='extra small size' borderRadius = '15px'></Input>
+                    <Input onChange = {changeStartTime} value = {startTime} type="date" color='#FFFFFFA6' placeholder='extra small size' borderRadius = '15px'></Input>
                     </Stack>
                     <Stack>
                     <FormLabel fontSize='sm' color='#FFFFFFA6'>End Time</FormLabel>
-                    <Input type="date" color='#FFFFFFA6' placeholder='extra small size' borderRadius = '15px'></Input>
+                    <Input onChange = {changeEndTime} value = {endTime} type="date" color='#FFFFFFA6' placeholder='extra small size' borderRadius = '15px'></Input>
                     </Stack>
                 </HStack>
                 <HStack>
                     <Stack>
                     <FormLabel fontSize='sm' color='#FFFFFFA6'>Floor Price</FormLabel>
-                    <Input type="number" color='#FFFFFFA6' placeholder='Starting bid price' borderRadius = '15px'></Input>
+                    <Input onChange = {changeFloorPrice} value = {floorPrice} type="number" color='#FFFFFFA6' placeholder='Starting bid price' borderRadius = '15px'></Input>
                     </Stack>
                     <Stack>
                     <FormLabel fontSize='sm' color='#FFFFFFA6'>Currency</FormLabel>
-                    <Input type="number" color='#FFFFFFA6' placeholder='Currency/Token' borderRadius = '15px'></Input>
+                    <Input onChange = {changeCurrency} value = {currency} type="string" color='#FFFFFFA6' placeholder='Currency/Token' borderRadius = '15px'></Input>
                     </Stack>
                 </HStack>
                 <FormLabel fontSize='sm' color='#FFFFFFA6'>Huddle Link</FormLabel>
-                <Input type="number" color='#FFFFFFA6' placeholder='Generate a huddle01 link' borderRadius = '15px'></Input>
+                <Input onChange = {changeHuddleLink} value = {eventLink} type="number" color='#FFFFFFA6' placeholder='Generate a huddle01 link' borderRadius = '15px'></Input>
                 <FormLabel fontSize='sm' color='#FFFFFFA6'>Custom URL</FormLabel>
-                <Input type="number" color='#FFFFFFA6' placeholder='Pick a custom URL' borderRadius = '15px'></Input>
+                <Input onChange = {changeCustomURL} value={customUrl} type="number" color='#FFFFFFA6' placeholder='Pick a custom URL' borderRadius = '15px'></Input>
             </FormControl>
             </Container>
             <Spacer/>
